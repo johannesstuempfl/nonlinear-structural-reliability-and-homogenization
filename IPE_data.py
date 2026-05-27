59b9d17286e7a789b764b5aa2d83b8ca44c8b9a7
johannesstuempfl <j.stuempfl@yahoo.com>
2026-05-27 11:32:58 +0200

    Added hard code constraints to FORM analysis

--- IPE_data.py
+++ IPE_data.py
@@ -5,7 +5,8 @@ I = 1.94e-5 # m4
 h = 0.2  # m
 z = h/2  # m
 sigma_yield = 46.0 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.14 # kNm
+alpha = 1.14
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
 # approximately resembles an IPE 160 
 E = 210e6 # kN/m2 
@@ -14,7 +15,8 @@ I = 0.869e-5 # m4
 h = 0.16  # m
 z = h/2  # m
 sigma_yield = 46.0 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.14 # kNm
+alpha = 1.14
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
 # approximately resembles an IPE 140 
 E = 210e6 # kN/m2 
@@ -23,7 +25,8 @@ I = 0.54e-5 # m4
 h = 0.14  # m
 z = h/2  # m
 sigma_yield = 46.0 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.14 # kNm
+alpha = 1.14
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
 # approximately resembles an IPE 120
 E = 210e6 # kN/m2 
@@ -32,16 +35,18 @@ I = 0.318e-5 # m4
 h = 0.12  # m
 z = h/2  # m
 sigma_yield = 35.5 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.14 # kNm
+alpha = 1.14
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
-# modified IPE 120 -> eta = 100%
+# modified IPE 120 -> eta Option 1 = 100%
 E = 210e6 # kN/m2 
 A = 1.321e-3 # m2
 I = 0.3093e-5 # m4
 h = 0.12  # m
 z = h/2  # m
-sigma_yield = 35.5 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.14 # kNm
+sigma_yield = 35.5 # m_k in kN/cm2 
+alpha = 1.14
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
 # approximately resembles an IPE 100
 E = 210e6 # kN/m2 
@@ -50,7 +55,8 @@ I = 0.17e-5 # m4
 h = 0.10  # m
 z = h/2  # m
 sigma_yield = 35.5 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.15 # kNm
+alpha = 1.15
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
 
 # modified IPE 100 (half frame)
 E = 210e6 # kN/m2 
@@ -59,4 +65,5 @@ I = 0.157e-5 # m4
 h = 0.10  # m
 z = h/2  # m
 sigma_yield = 42.0 # kN/cm2 
-M_yield = I/z * sigma_yield * 100e2 * 1.15 # kNm
+alpha = 1.15
+M_yield = I/z * sigma_yield * 100e2 * alpha # kNm
